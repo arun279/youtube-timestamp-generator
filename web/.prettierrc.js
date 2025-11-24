2537bd41895b0dcd0de9340e499532962a2d4575
Arun Krishnamurthy <arun.km.279@gmail.com>
2025-11-23 21:50:29 -0600

    - Introduced .editorconfig for consistent coding styles across the project. - Added .prettierignore to exclude specific files from formatting. - Created .husky/pre-commit script to enforce linting on pre-commit. - Updated package.json to include new linting and formatting scripts. - Added package-lock.json to manage dependencies and ensure reproducibility. - Updated .gitignore to include Husky-related files.

---
+++ web/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
